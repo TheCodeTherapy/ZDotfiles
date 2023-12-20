9560ac599306f706799d1c34d98034e108b9b27f
TheCodeTherapy <me@mgz.me>
2023-12-20 11:45:58 +0000

    updates stuff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,5 +3,5 @@
 -- Add any additional options here
 
 -- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
+-- vim.cmd([[let &t_Cs = "\e[4:3m"]])
+-- vim.cmd([[let &t_Ce = "\e[4:0m"]])
