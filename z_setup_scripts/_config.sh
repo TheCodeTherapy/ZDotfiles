8c8d8d80769eb4c4d1bad27eb3ef941ea55048ff
TheCodeTherapy <me@mgz.me>
2024-09-06 23:56:31 +0100

    updates global config variables

--- z_setup_scripts/_config.sh
+++ z_setup_scripts/_config.sh
@@ -5,10 +5,16 @@ ME="/home/$(whoami)"
 export ME
 export DOTDIR="${ME}/ZDotfiles"
 export DOTDOT="${DOTDIR}/dotfiles"
+
+export BINDIR="${DOTDIR}/bin"
+export SCRIPTS="${DOTDIR}/scripts"
+export SETUPSCRIPTS="${DOTDIR}/z_setup_scripts"
+
 export NVMDIR="${ME}/.nvm"
 export DOTLOCAL="${ME}/.local"
-export BINDIR="${DOTDIR}/bin"
 export CFG="$ME/.config"
+export GAMES="$ME/Games"
+
 export HOSTSBACKUP=/etc/hosts.bak
 export HOSTSDENYBACKUP=/etc/hostsdeny.bak
 export HOSTSSECURED="${DOTDIR}/hostssecured"
