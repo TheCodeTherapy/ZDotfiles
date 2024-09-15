e3e0b476dfca4a8e61bc7badc52ff16290131004
TheCodeTherapy <me@mgz.me>
2024-09-15 14:41:04 +0100

    updates setup script

--- setup.sh
+++ setup.sh
@@ -13,48 +13,64 @@ update_system() {
 }
 
 install_basic_packages() {
-  print_info "Installing basic packages ..."
   local packages=(
     plocate build-essential llvm pkg-config autoconf automake cmake cmake-data
     autopoint ninja-build gettext libtool libtool-bin g++ make meson clang nasm
-    clang-tools ca-certificates curl gnupg lsb-release python-is-python3 feh
-    ipython3 python3-pip python3-dev python3-venv gawk unzip lzma tree neofetch
-    python3-gi gir1.2-gtk-3.0 python3-gi-cairo python3-cairo python3-setuptools
-    python3-babel python3-dbus playerctl fonts-font-awesome slop dunst timidity
-    python3-pynvim python3-sphinx python3-packaging xclip notification-daemon
-    git git-lfs zsh tmux gnome-tweaks inxi most ttfautohint v4l2loopback-dkms
-    ffmpeg htop bc fzf ranger ripgrep gdebi nautilus-admin tar jq pixz
-    screenkey mypaint rofi gimp blender imagemagick net-tools xcb-proto
-    gir1.2-ayatanaappindicator3-0.1 nautilus-extension-gnome-terminal
-    gnome-shell-extension-manager policykit-1-gnome uthash-dev hashdeep
-    libnotify-bin libfontconfig1-dev libfreetype-dev libxext-dev
-    liblxc-dev libxrandr-dev libxinerama-dev libxcursor-dev libglx-dev
-    libgl-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev
-    libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev
-    libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev
-    libxcb-shape0-dev libxcb-xrm-dev libxcb-xrm0 libxcb-xkb-dev
-    libconfig-dev libdbus-1-dev libegl-dev libpcre2-dev libpixman-1-dev
-    libx11-xcb-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev
-    libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-render0-dev
-    libxcb-render-util0-dev libxcb-util-dev libxcb-xfixes0-dev
-    libxkbcommon-dev libxkbcommon-x11-dev xutils-dev asciidoc libconfuse-dev
-    libasound2-dev libiw-dev libpulse-dev libnl-genl-3-dev libuv1-dev
-    libcairo2-dev python3-xcbgen libxcb-ewmh-dev libjsoncpp-dev
-    libmpdclient-dev libcurl4-openssl-dev libgtk-4-dev libx11-dev
-    libxcomposite-dev libxfixes-dev libgl1-mesa-dev libxi-dev libwayland-dev
-    libncurses5-dev libreadline-dev usbview v4l-utils libxrender-dev
-    libglew-dev libsdl2-dev zlib1g-dev libz3-dev libjpeg-dev libfluidsynth-dev
-    libgme-dev libopenal-dev libopenal-data libmpg123-dev libsndfile1-dev
-    libgtk-3-dev libsdl1.2-dev
+    clang-tools curl wget ca-certificates gnupg lsb-release feh gawk unzip lzma
+    xclip notification-daemon git git-lfs zsh tmux gnome-tweaks inxi most tree
+    neofetch playerctl fonts-font-awesome slop dunst timidity gir1.2-gtk-3.0
+    ttfautohint v4l2loopback-dkms ffmpeg htop bc fzf ranger ripgrep gdebi
+    nautilus-admin tar jq pixz screenkey mypaint rofi gimp blender
+    imagemagick net-tools xcb-proto gir1.2-ayatanaappindicator3-0.1
+    nautilus-extension-gnome-terminal asciidoc gnome-shell-extension-manager
+    policykit-1 policykit-1-gnome uthash-dev hashdeep file usbview v4l-utils
+    python-is-python3 ipython3 python3-pip python3-dev python3-venv
+    python3-gi python3-gi-cairo python3-cairo python3-setuptools
+    python3-babel python3-dbus python3-pynvim python3-sphinx python3-packaging
+    python3-xcbgen xutils-dev xwayland valac hwdata
   )
 
+  print_info "Installing basic packages ..."
   if ! sudo debconf-apt-progress -- apt-get install -y "${packages[@]}"; then
-    handle_error "Failed to install some packages."
+    handle_error "Failed to install one or more packages."
   fi
+}
 
-  #for package in "${packages[@]}"; do
-  #  install_with_package_manager "$package"
-  #done
+install_dev_libs() {
+  local devlibs=(
+    libwayland-dev libffi-dev libxml2-dev libegl1-mesa-dev libgles2-mesa-dev
+    libgbm-dev libinput-dev libglm-dev libgtkmm-3.0-dev libdrm-dev
+    libgirepository1.0-dev libsystemd-dev libxcb-xinput-dev libseat-dev
+    libdbusmenu-gtk3-dev libxkbregistry-dev libdisplay-info-dev
+    libev-dev libgtk-3-dev libgtk-4-dev libwebkit2gtk-4.1-dev libsdl1.2-dev
+    libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev
+    libnotify-bin libfontconfig1-dev libfreetype-dev libxext-dev liblxc-dev
+    libxrandr-dev libxinerama-dev libxcursor-dev libglx-dev libgl-dev
+    libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev
+    libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev
+    libxcb-randr0-dev libxcb-cursor-dev libxcb-xinerama0-dev
+    libxcb-shape0-dev libxcb-xrm-dev libxcb-xrm0 libxcb-xkb-dev libconfig-dev
+    libx11-xcb-dev libdbus-1-dev libegl-dev libpcre2-dev libpixman-1-dev
+    libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev
+    libxcb-image0-dev libxcb-present-dev libxcb-render0-dev
+    libxcb-render-util0-dev libxcb-util-dev libxcb-xfixes0-dev libxkbcommon-dev
+    libxcb-ewmh-dev libnl-genl-3-dev libuv1-dev libcairo2-dev libjsoncpp-dev
+    libxkbcommon-x11-dev libconfuse-dev libasound2-dev libiw-dev libpulse-dev
+    libmpdclient-dev libcurl4-openssl-dev libx11-dev libxcomposite-dev
+    libxfixes-dev libgl1-mesa-dev libxi-dev libwayland-dev libncurses5-dev
+    libreadline-dev libxrender-dev libglew-dev libsdl2-dev libz3-dev
+    libjpeg-dev libfluidsynth-dev libgme-dev libopenal-dev libopenal-data
+    libmpg123-dev libsndfile1-dev zlib1g-dev
+  )
+
+  print_info "Installing dev libs ..."
+  if ! sudo debconf-apt-progress -- apt-get install -y "${devlibs[@]}"; then
+    handle_error "Failed to install one or more dev libs."
+  fi
+}
+
+update_plocate_db() {
+  print_info "Updating plocate database ..."
   sudo updatedb || handle_error "Failed to update the file database."
 }
 
@@ -163,6 +179,9 @@ update_cache() {
 update_system
 
 install_basic_packages
+install_dev_libs
+update_plocate_db
+
 install_recipes
 install_snap_packages
 
