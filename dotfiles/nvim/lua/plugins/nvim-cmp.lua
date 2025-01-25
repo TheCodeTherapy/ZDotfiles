6dba0c02853676ea6853079c502482a1901c5411
TheCodeTherapy <me@mgz.me>
2025-01-25 15:02:05 +0000

    explicit cmp options

---
+++ dotfiles/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    window = {
+      completion = {
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+      },
+      documentation = {
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+      },
+    },
+  },
+}
