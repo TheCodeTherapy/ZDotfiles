d7cbcde6947bff411282ddd05f15612fc879e98a
TheCodeTherapy <me@mgz.me>
2025-02-06 16:35:11 +0000

    updates lsp config to avoid clang conflicts with copilot

--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/plugins/lspconfig.lua
@@ -66,6 +66,11 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
+        clangd = {
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+        },
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           -- Use this to add any additional keymaps
