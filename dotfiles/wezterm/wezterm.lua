db0be762216a9b57b876f22a3d4e6de9603b3f0c
TheCodeTherapy <me@mgz.me>
2025-04-22 15:22:55 +0100

    updates wezterm config

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -3,6 +3,18 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
+config.window_background_opacity = 0.85
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 999999
+config.window_padding = {
+  left = 30,
+  right = 30,
+  top = 30,
+  bottom = 30,
+}
+config.window_decorations = "RESIZE"
 
 return config
