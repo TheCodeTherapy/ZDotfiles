72b4f352fe4e3b83db7a084aede84ac986217037
TheCodeTherapy <me@mgz.me>
2025-04-24 01:11:42 +0100

    updates fonts

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
-config.font_size = 14.0
+config.font = wezterm.font("ComicShannsMono Nerd Font Mono", { weight = "Medium" })
+config.font_size = 15.0
 config.window_background_opacity = 0.9
 config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = true
@@ -18,7 +18,7 @@ config.tab_max_width = 999999
 -- }
 config.window_decorations = "RESIZE"
 config.window_frame = {
-  font = wezterm.font { family = "JetBrainsMono Nerd Font", weight = "Bold" },
+  font = wezterm.font { family = "ComicShannsMono Nerd Font Mono", weight = "Bold" },
   font_size = 12.0,
   active_titlebar_bg = "#1e1e2e",
   inactive_titlebar_bg = "#1e1e2e",
