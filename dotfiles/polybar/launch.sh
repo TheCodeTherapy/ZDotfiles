df1a4fc675b14585cfab088785c59cedb1789fa5
TheCodeTherapy <me@mgz.me>
2025-04-25 11:57:30 +0100

    updates polybar config

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
 if type "xrandr"; then
-    # for MON in $(xrandr --query | grep " connected" | grep -v "primary" | grep -v "None" | cut -d " " -f 1); do
-    #     MONITOR=$MON polybar -c ~/.config/polybar/config.ini main &
-    # done
-    # for now I'll run it only on the left-most monitor
-    for MON in $(xrandr --query | grep " connected" | grep -v "primary" | grep -v "None" | grep "7680" | cut -d " " -f 1); do
-        MONITOR=$MON polybar -c ~/.config/polybar/config.ini main &
-    done
+    MON=$(xrandr --query | grep " connected" | grep "primary" | cut -d " " -f 1)
+    MONITOR=$MON polybar -c ~/.config/polybar/config.ini main &
 else
+    echo "could not find xrandr, launching default polybar"
     polybar -c ~/.config/polybar/config.ini main &
 fi
